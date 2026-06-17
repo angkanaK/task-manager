a4cc6895fb0eafe77d8c577ceac1b2e7f35794b8
angkanaK <angkanawarangka.k@gmail.com>
2026-06-17 15:11:15 +0700

    switch to postgresql

--- backend/database.py
+++ backend/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:@127.0.0.1:3306/taskmanager"
+DATABASE_URL = "postgresql://task_manager_db_vgec_user:USbys2FkA4gYoXy3e9pt0gLvVVxiKcmi@dpg-d8p5ai1194ac738uufgg-a/task_manager_db_vgec"
 
 engine = create_engine(DATABASE_URL)
 
